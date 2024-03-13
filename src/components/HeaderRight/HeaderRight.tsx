ce89f78d7fbe0f4e2054e797f3613f8e0936df47
PokizaSdv <pokizasdv@gmail.com>
2024-03-13 00:57:19 -0400

    Add background to img

--- src/components/HeaderRight/HeaderRight.tsx
+++ src/components/HeaderRight/HeaderRight.tsx
@@ -3,8 +3,10 @@ import heroImg from "../../assets/hero3.png";
 
 const HeaderRight = () => {
     return (
-        <div className="header-right" >
-            <img src={heroImg} alt="Woman" />
+        <div className="header-right">
+            <div className="img-wrapper">
+                <img src={heroImg} alt="Woman" />
+            </div>
         </div>
     );
 };
