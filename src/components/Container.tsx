bb03a4b14d9b853e4f71cade5a23cf60b318e08d
PokizaSdv <pokizasdv@gmail.com>
2024-03-31 13:55:10 -0400

    Restructure Container and create FileViewer component

---
+++ src/components/Container.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    max-width: 144rem;
+    display: flex;
+    overflow: hidden;
+    padding: 0 var(--space-100) var(--space-100) var(--space-100);
+`;
